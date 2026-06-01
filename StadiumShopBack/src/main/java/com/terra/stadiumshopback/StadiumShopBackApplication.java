045f143196136323bd241618173af4f5e4f6902f
Terra <djakiss.terra100@gmail.com>
2026-06-01 16:08:04 +0000

    initialisation back

---
+++ StadiumShopBack/src/main/java/com/terra/stadiumshopback/StadiumShopBackApplication.java
@@ -0,0 +1,13 @@
+package com.terra.stadiumshopback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StadiumShopBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StadiumShopBackApplication.class, args);
+    }
+
+}
---
+++ StadiumShopBack/src/test/java/com/terra/stadiumshopback/StadiumShopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terra.stadiumshopback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StadiumShopBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
